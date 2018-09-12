bcc8d347749d1595ad90deaef83be92f97af24ae
pvtnutty <rcontreras@wiggot.com>
2018-09-12 12:14:54 -0500

    Added roles and ACL's to container model

--- server/boot/roles.js
+++ server/boot/roles.js
@@ -1,4 +1,39 @@
 module.exports = function(app) {
-  const { Role } = app.models;
-  Role.registerResolver("viewer", (role, ctx, cb) => {});
+  const { AppUser, Role } = app.models;
+  Role.registerResolver("VIEWER", (role, ctx, cb) => {
+    if (ctx.accessToken.userId) {
+      validatesUserType(ctx, "VIEWER", ctx.accessToken.userId)
+        .then(res => {
+          cb(null, res);
+        })
+        .catch(e => {
+          cb(e);
+        });
+    }
+  });
+
+  Role.registerResolver("EDITOR", (role, ctx, cb) => {
+    if (ctx.accessToken.userId) {
+      validatesUserType(ctx, "EDITOR", ctx.accessToken.userId)
+        .then(res => {
+          cb(null, res);
+        })
+        .catch(e => {
+          cb(e);
+        });
+    }
+  });
+
+  async function validatesUserType(ctx, role, id) {
+    try {
+      const appUser = await AppUser.findById(id);
+      if (appUser.userType === role) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (error) {
+      throw error;
+    }
+  }
 };
